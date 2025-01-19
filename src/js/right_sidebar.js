c4859f4ef14a2a6458d6866831484cc21c4e505c
Bogdan Titov <bonya.titov@yandex.ru>
2025-01-19 18:16:22 +0300

    fix 1440px commit

--- src/js/right_sidebar.js
+++ src/js/right_sidebar.js
@@ -6,7 +6,12 @@
   const buttonPhoneMenu = document.querySelector('.contacts__link-phone');
   const sidebar = document.querySelector('.sidebar-container');
   const buttonPhone = document.getElementById('button-phone');
-  
+  const collBackBtn = document.querySelectorAll('.call_back');
+  const form = document.querySelector('.form');
+  const formLabel = form.querySelectorAll('.form__label');
+  const headerHeading = document.querySelector('.header__heading');
+
+
   if (rightSidebar && rightContainer) {
     rightContainer.addEventListener('click', function(evt) {
       if (evt.target === this) {
@@ -25,12 +30,26 @@
 
 
   buttonPhone.addEventListener('click', function() {
+    formLabel.forEach(label => {label.classList.remove('closed')});
+    headerHeading.textContent = 'Обратная связь';
     rightContainer.classList.remove('closed');
   });
 
   buttonPhoneMenu.addEventListener('click', function() {
+    formLabel.forEach(label => {label.classList.remove('closed')});
+    headerHeading.textContent = 'Обратная связь';
     sidebar.classList.add('closed');
     rightContainer.classList.remove('closed');
+  });
+
+
+  collBackBtn.forEach(btn => {
+    btn.addEventListener('click', function() {
+      sidebar.classList.add('closed');
+      rightContainer.classList.remove('closed');
+      headerHeading.textContent = 'Заказать звонок';
+      formLabel.forEach(label => {label.classList.add('closed')});
+    });
   });
 
 
